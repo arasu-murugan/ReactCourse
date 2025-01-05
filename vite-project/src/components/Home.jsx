81d253764927854f4156ae5fa5c126db26c8c1d0
arasu <arasumurugan1903@gmail.com>
2025-01-05 21:29:17 +0530

    home

--- vite-project/src/components/Home.jsx
+++ vite-project/src/components/Home.jsx
@@ -5,11 +5,7 @@ const Home = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">MyWebsite</div>
-      <ul className="navbar-links">
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+      <h1>This is Home Page</h1>
     </nav>
   );
 };
